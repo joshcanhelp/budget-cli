74cd851d210a9efe30ff8ceee9225382af452e79
Josh Cunningham <josh@joshcanhelp.com>
2024-02-26 08:04:54 -0800

    test: fix module import in tests
    https://stackoverflow.com/a/69598249

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,11 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ["./src"]
+  roots: ["./src"],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  }
 };
 
 export default config;
